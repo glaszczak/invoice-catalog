fca556d74f021c2c5f4e0407052d5f3d79940637
Grzegorz <grzegorz.laszczak@gmail.com>
2023-10-25 22:53:50 +0200

    fix missing process variables

--- src/invoices/invoices.module.ts
+++ src/invoices/invoices.module.ts
@@ -2,22 +2,31 @@ import { Module } from '@nestjs/common';
 import { InvoicesService } from './invoices.service';
 import { InvoicesController } from './invoices.controller';
 import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    ClientsModule.register([
+    ClientsModule.registerAsync([
       {
         name: 'RABBITMQ_CLIENT',
-        transport: Transport.RMQ,
-        options: {
-          urls: [
-            `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_HOST}:5672`,
-          ],
-          queue: process.env.RABBITMQ_QUEUE_NAME,
-          queueOptions: {
-            durable: false,
+        imports: [ConfigModule],
+        useFactory: async (configService: ConfigService) => ({
+          transport: Transport.RMQ,
+          options: {
+            urls: [
+              `amqp://${configService.get(
+                'RABBITMQ_DEFAULT_USER',
+              )}:${configService.get(
+                'RABBITMQ_DEFAULT_PASS',
+              )}@${configService.get('RABBITMQ_HOST')}:5672`,
+            ],
+            queue: configService.get('RABBITMQ_QUEUE_NAME'),
+            queueOptions: {
+              durable: false,
+            },
           },
-        },
+        }),
+        inject: [ConfigService],
       },
     ]),
   ],
