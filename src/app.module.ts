dc600f8df242797d6984320e557a8f0df0f1035d
Grzegorz <grzegorz.laszczak@gmail.com>
2023-10-25 23:12:07 +0200

    remove unused service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { HealthModule } from './health/health.module';
 import { InvoiceModule } from './invoices/invoices.module';
 import { ConfigModule } from '@nestjs/config';
@@ -14,6 +13,5 @@ import { ConfigModule } from '@nestjs/config';
     InvoiceModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
