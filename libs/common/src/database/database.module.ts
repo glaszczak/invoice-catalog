52f1368f13a1ba01eab16746f2e4fcfa271410c3
Grzegorz <grzegorz.laszczak@gmail.com>
2023-11-01 16:49:55 +0100

    fix mongodb connection

--- libs/common/src/database/database.module.ts
+++ libs/common/src/database/database.module.ts
@@ -1,15 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
+import { ModelDefinition, MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forRootAsync({
-      useFactory: async (configService: ConfigService) => ({
-        uri: configService.get('MONGODB_URI'),
-      }),
+      useFactory: async (configService: ConfigService) => {
+        const host = configService.get('MONGODB_HOST');
+        const port = configService.get('MONGODB_PORT');
+        const database = configService.get('MONGODB_DATABASE');
+
+        return { uri: `mongodb://${host}:${port}/${database}` };
+      },
       inject: [ConfigService],
     }),
   ],
 })
-export class DatabaseModule {}
+export class DatabaseModule {
+  static forFeature(models: ModelDefinition[]) {
+    return MongooseModule.forFeature(models);
+  }
+}
